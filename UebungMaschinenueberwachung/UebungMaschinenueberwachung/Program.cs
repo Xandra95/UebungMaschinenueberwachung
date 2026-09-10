757040417a62630b0ff9cc22a833ab3cb1745dc4
Krämer, Alexandra <a.kraemer@hawe.de>
2026-09-10 22:09:22 +0200

    Funktion 1 erfasst

--- UebungMaschinenueberwachung/UebungMaschinenueberwachung/Program.cs
+++ UebungMaschinenueberwachung/UebungMaschinenueberwachung/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace UebungMaschinenueberwachung
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace UebungMaschinenueberwachung
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ZeigeStartseite();
         }
+        static void ZeigeStartseite()
+        {
+            List<string> ZeigeStartseite = new List<string>
+            {
+                "1 - Temperatur erfassen",
+                "2 - Statisik erfassen",
+                "3 - Grenzwerte prüfen",
+                "0 - Programm beenden"
+            };
+
+            foreach(var erstes in ZeigeStartseite)
+            {
+                Console.WriteLine(erstes);
+            }
+
+            TemperaturAngaben();
+
+            int temperatur = 1;
+            int statisik = 2;
+            int grenzwert = 3;
+            int beenden = 0;
+        }
+        
+            
+
+
+        
     }
 }
