35263b8c648e2f03d097667a137413b0c023c7d7
Krämer, Alexandra <a.kraemer@hawe.de>
2026-09-10 13:24:04 +0200

    Erstellen der Uebung Maschinenbewachung

---
+++ UebungMaschinenueberwachung/UebungMaschinenueberwachung/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UebungMaschinenueberwachung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
