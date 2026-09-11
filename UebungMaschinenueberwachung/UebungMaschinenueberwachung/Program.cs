ecfcf0af54abd476561f051e9430fd29a8181119
Krämer, Alexandra <a.kraemer@hawe.de>
2026-09-11 14:03:34 +0200

    Aufgabe 1 erledigt - ohne Glühbirne

--- UebungMaschinenueberwachung/UebungMaschinenueberwachung/Program.cs
+++ UebungMaschinenueberwachung/UebungMaschinenueberwachung/Program.cs
@@ -8,39 +8,67 @@ namespace UebungMaschinenueberwachung
         static void Main(string[] args)
         {
             ZeigeStartseite();
+
+            static void ZeigeStartseite()
+            {
+                List<string> ZeigeStartseite = new List<string>
+                {
+                    "1 - Temperatur erfassen",
+                    "2 - Statisik erfassen",
+                    "3 - Grenzwerte prüfen",
+                    "0 - Programm beenden"
+                };
+
+
+                foreach (var erstes in ZeigeStartseite)
+                {
+                    Console.WriteLine(erstes);
+                }
+
+                int temperatur = 1;
+                int statistik = 2;
+                int grenzwert = 3;
+                int beenden = 0;
+
+                int eingegebeneZahl = Convert.ToInt32(Console.ReadLine());
+
+                StartEingabe(eingegebeneZahl);
+
+            }
+
         }
-        static void ZeigeStartseite()
+
+        static void StartEingabe(int eingegebeneZahl)
         {
-            List<string> ZeigeStartseite = new List<string>
-            {
-                "1 - Temperatur erfassen",
-                "2 - Statisik erfassen",
-                "3 - Grenzwerte prüfen",
-                "0 - Programm beenden"
-            };
 
-            foreach(var erstes in ZeigeStartseite)
+            if (eingegebeneZahl == 1)
             {
-                Console.WriteLine(erstes);
+                Console.WriteLine("Bitte Temperaturwerte eingeben");
+                int erfassung = Convert.ToInt32(Console.ReadLine());
+
             }
-
-            StartEingabe();
-
-            int temperatur = 1;
-            int statisik = 2;
-            int grenzwert = 3;
-            int beenden = 0;
-
-            static void StartEingabe()
+            else if (eingegebeneZahl == 2)
             {
-                Console.ReadLine();
+                Console.WriteLine("Hier ist Ihre Statistik zu den Temperaturwerten ");
             }
+            else if (eingegebeneZahl == 3)
+            {
+                Console.WriteLine("Grenzwerte wurden geprüft: ");
+            }
+            else
+            {
 
+            }
         }
-        
-            
 
 
         
     }
 }
+       
+        
+            
+
+
+        
+    
